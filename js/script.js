eb18c57d131f03e8dcd3501e47bfef2af9e9d642
antoniostassi <stassiant@outlook.it>
2024-07-24 16:19:08 +0200

    JS | Timeout inside function to ensure the change of the html.

--- js/script.js
+++ js/script.js
@@ -45,30 +45,31 @@ function askNumbers(){
 
         timerTag.innerHTML = "<p><strong>"+ 0 +"</p></strong>";
         divToMemorize.innerHTML = ""; // Nascondi i numeri
-        htmlIsRemoved = true;   
-        setTimeout(askForPrompt(), 1000);
 
-        console.log(userNumbers); // Stampo l'array contenente gli input dell'utente.
-
-        for (let k=0; k < userNumbers.length; k++) { 
-            if (!randomNumbers.includes(userNumbers[k])) { // Se almeno un numero non è contenuto nell'array dei numeri da ricordare
-                alert("Hai perso!"); // Allora hai perso
-                gameOver = true; // gameOver status == true;
-                break; // Ferma il ciclo
+        setTimeout(function(){
+            for (let k=0; k < 5; k++) {
+                let userNum = parseInt(prompt((k+1) + " - Indovina un numero:")); // Chiedo gli input all'utente.
+                userNumbers.push(userNum); // Salvo tutti gli input dell'utente.
             }
-        }
 
-        if(gameOver == false) { // Se non hai perso, gameOver è ancora = false, dunque
-            alert("Hai vinto!"); // Hai Vinto
-        }
+            console.log(userNumbers); // Stampo l'array contenente gli input dell'utente.
 
-        location.reload();
-    }
-}
+            for (let k=0; k < userNumbers.length; k++) { 
+                if (!randomNumbers.includes(userNumbers[k])) { // Se almeno un numero non è contenuto nell'array dei numeri da ricordare
+                    alert("Hai perso!"); // Allora hai perso
+                    gameOver = true; // gameOver status == true;
+                    break; // Ferma il ciclo
+                }
+            }
 
-function askForPrompt() {
-    for (let k=0; k < 5; k++) {
-        let userNum = parseInt(prompt((k+1) + " - Indovina un numero:")); // Chiedo gli input all'utente.
-        userNumbers.push(userNum); // Salvo tutti gli input dell'utente.
+            if(gameOver == false) { // Se non hai perso, gameOver è ancora = false, dunque
+                alert("Hai vinto!"); // Hai Vinto
+            }
+
+            location.reload();
+
+        }, 100);
+
+        
     }
 }
